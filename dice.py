57fe7d6e6cc0606ba7a2b8396728df21c4e20070
mattyImry <matteo.imeri@gmail.com>
2021-11-30 14:20:57 +0000

    Added comments to dice.py file and add to notebook.md

--- dice.py
+++ dice.py
@@ -1,9 +1,10 @@
+"""Importing Random module."""
 import random
 
 
 def dice_match_2():
     """
-        rolling dice checking double mathing double value
+        rolling dice checking two of a kind.
     """
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
@@ -11,53 +12,53 @@ def dice_match_2():
     dice4 = random.randint(1, 6)
     dice5 = random.randint(1, 6)
 
-    print("Dice result are " + str(dice1), str(dice2), str(dice3),
+    print("Dice results are " + str(dice1), str(dice2), str(dice3),
           str(dice4), str(dice5))
 
     match = (dice1 == 1) + (dice2 == 1) + (dice3 == 1)\
         + (dice4 == 1) + (dice5 == 1)
 
     if match == 2:
-        print("2 out of 5 have same value")
+        print("Two of a kind")
 
     else:
         match = (dice1 == 2) + (dice2 == 2) + (dice3 == 2)\
                 + (dice4 == 2) + (dice5 == 2)
         if match >= 2:
-            print("2 out of 5 have same value")
+            print("Two of a kind")
 
         else:
             match = (dice1 == 3) + (dice2 == 3) + (dice3 == 3)\
                     + (dice4 == 3) + (dice5 == 3)
             if match >= 2:
-                print("2 out of 5 have same value")
+                print("Two of a kind")
 
             else:
                 match = (dice1 == 4) + (dice2 == 4) + (dice3 == 4)\
                         + (dice4 == 4) + (dice5 == 4)
                 if match >= 2:
-                    print("2 out of 5 have same value")
+                    print("Two of a kind")
 
                 else:
                     match = (dice1 == 5) + (dice2 == 5) + (dice3 == 5)\
                             + (dice4 == 5) + (dice5 == 5)
                     if match >= 2:
-                        print("2 out of 5 have same value")
+                        print("Two of a kind")
 
                     else:
                         match = (dice1 == 6) + (dice2 == 6) + (dice3 == 6)\
                                 + (dice4 == 6) + (dice5 == 6)
                         if match >= 2:
-                            print("2 out of 5 have same value")
+                            print("Two of a kind")
 
                         else:
-                            print("No double matching value")
+                            print("No two of a kind")
 
 
 def dice_match_3():
 
     """
-        rolling dice checking double mathing double value
+        rolling dice checking double mathing triple value
     """
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
@@ -65,47 +66,49 @@ def dice_match_3():
     dice4 = random.randint(1, 6)
     dice5 = random.randint(1, 6)
 
-    print("Dice result are " + str(dice1), str(dice2), str(dice3),
+    print("Dice results are " + str(dice1), str(dice2), str(dice3),
           str(dice4), str(dice5))
 
     match = (dice1 == 1) + (dice2 == 1) + (dice3 == 1)\
         + (dice4 == 1) + (dice5 == 1)
     if match == 3:
-        print("3 out of 5 have same value")
+        print("Three of a kind")
 
     else:
         match = (dice1 == 2) + (dice2 == 2) + (dice3 == 2)\
                 + (dice4 == 2) + (dice5 == 2)
         if match >= 3:
-            print("3 out of 5 have same value")
+            print("Three of a kind")
 
         else:
             match = (dice1 == 3) + (dice2 == 3) + (dice3 == 3)\
                     + (dice4 == 3) + (dice5 == 3)
             if match >= 3:
-                print("3 out of 5 have same value")
+                print("Three of a kind")
 
             else:
                 match = (dice1 == 4) + (dice2 == 4) + (dice3 == 4)\
                         + (dice4 == 4) + (dice5 == 4)
                 if match >= 3:
-                    print("3 out of 5 have same value")
+                    print("Three of a kind")
 
                 else:
                     match = (dice1 == 5) + (dice2 == 5) + (dice3 == 5)\
                             + (dice4 == 5) + (dice5 == 5)
                     if match >= 3:
-                        print("3 out of 5 have same value")
+                        print("Three of a kind")
 
                     else:
                         match = (dice1 == 6) + (dice2 == 6) + (dice3 == 6)\
                                 + (dice4 == 6) + (dice5 == 6)
                         if match >= 3:
-                            print("3 out of 5 have same value")
+                            print("Three of a kind")
 
                         else:
-                            print("No triple matching value")
+                            print("No three of a kind")
 
-
+"""
+    Calling functions
+"""
 dice_match_2()
 dice_match_3()
--- notebook.md
+++ notebook.md
@@ -3,8 +3,18 @@
 Here you can colllect notes about your thought process, or other
 things you think we'll be interested in.
 
+Steps:
 
 1- Create 5 dice with 6 faces
 2- The dice has to create random numbers from 1 to 6.
 3- Import needed `from random import randint`
 4- Check if there is two-of-a-kind number after roll
+5- Check if there is three-of-a-kind number after roll
+6- Code refactoring(Not done)
+7- Testing (Not done)
+
+Extra notes:
+
+I have managed to check if there is a two-of-a-kind and three-of-a-kind. I wanted to try to refactoring code and test the functions.
+I did runout of time as by doing that I would have use more time than suggested.
+I did not used any additional dependences for this test.
